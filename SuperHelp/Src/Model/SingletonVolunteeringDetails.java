a521b911e0a405af628aa4726c243a8d34231206
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2021-08-19 12:46:55 +0300

    Create SingletonVolunteeringDetails

---
+++ SuperHelp/Src/Model/SingletonVolunteeringDetails.java
@@ -0,0 +1,63 @@
+package Model;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingletonVolunteeringDetails {
+    private static SingletonVolunteeringDetails firstInstance = new SingletonVolunteeringDetails();
+
+    private final String _url = "https://www.science.co.il/municipal/Cities.php";
+    private List<Integer> _ages = setAge();
+    private List<String> _locations = setLocation();
+    private final String[] _gender = {"male", "female"};
+
+    private SingletonVolunteeringDetails() { }
+
+    public static SingletonVolunteeringDetails getInstance() {
+        return firstInstance;
+    }
+
+    private List<Integer> setAge() {
+        List<Integer> ages = new ArrayList<Integer>();
+        for (int i = 16; i <= 120; i++) {
+            ages.add(i);
+        }
+        return ages;
+    }
+
+    private List<String> setLocation() {
+        List<String> locatinos = new ArrayList<>();
+
+        try {
+            Document doc = Jsoup.connect("https://www.science.co.il/municipal/Cities.php").get();
+            Elements table = doc.getElementsByClass("content");
+            table.forEach(city -> {
+                city.getElementsByTag("a").forEach(el -> {
+                    locatinos.add(el.text());
+                });
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return locatinos;
+    }
+
+    public List<Integer> getAge() {
+        return _ages;
+    }
+
+    public List<String> getLocation() {
+        return _locations;
+    }
+    public String[] getGender() {
+        return _gender;
+    }
+}
